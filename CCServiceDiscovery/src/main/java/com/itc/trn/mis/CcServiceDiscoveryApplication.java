168ac9d1e9907e220680c3973e3a01dbc764c97d
SEBUJAS <SEBUJAS@SEBU>
2019-08-02 13:00:15 +0530

    1

---
+++ CCServiceDiscovery/src/main/java/com/itc/trn/mis/CcServiceDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.itc.trn.mis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CcServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcServiceDiscoveryApplication.class, args);
+	}
+
+}
